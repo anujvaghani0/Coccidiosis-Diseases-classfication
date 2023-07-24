dceba3f5bd37d0c43c8fe3f1cc431258424e0d72
anujvaghani0 <anujvaghani0@gmail.com>
2023-07-24 16:28:53 +0530

    dvc added

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -2,6 +2,18 @@ from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger
 
+# ssl setup for mac
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+    
 
 STAGE_NAME = "Data Ingestion stage"
 
